99b8713897d1be9cea5df908bdde753394ad8f98
Chris Sanders <astericky@gmail.com>
2016-09-14 14:29:02 -0400

    Add static todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import TodoList from 'TodoList'
 
 class TodoApp extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        },
+        {
+          id: 3,
+          text: 'Write the react'
+        },
+        {
+          id: 4,
+          text: 'Build the app'
+        }
+      ]
+    }
+  }
+
   render () {
+    let { todos } = this.state
+
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos} />
       </div>
     )
   }
